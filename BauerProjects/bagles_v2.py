30da443a524de6cd6fc99c475b4a5400ba4b361d
kasiabauer <bauer.kasia@gmail.com>
2023-05-05 22:04:34 +0200

    added get_feedback() function added prompts in english - game has a Piko bug needed to be fixed

--- BauerProjects/bagles_v2.py
+++ BauerProjects/bagles_v2.py
@@ -12,13 +12,13 @@ MAX_NUMBERS = 3
 #Inform the user about the game
 user_input = input(f'''
 Hello gamer!
-This is text based game: BAGLES
+This is text based game: BEAGLES
 You have 10 tries to guess a number.
 Number will consist of {MAX_NUMBERS} numbers. Example: 023 or 359
-After each answer you'll recieve a podpowiedź. 
-PIKO: One number is correct but in a wrong position.
-FERMI: One number is correct but in a correct position.
-BAGLES: None of numbers are correct.
+After each answer you'll receive a response. 
+\tPIKO: One number is correct but in a wrong position.
+\tFERMI: One number is correct and in a correct position.
+\tBEAGLES: None of numbers are correct nor in a correct position.
 Would you like to play? 
 (yes / no): \n
 ''')
@@ -37,20 +37,44 @@ def generate_number():
 user_tries = 1
 number_to_guess = generate_number()
 print(number_to_guess)
-prompt_failed = 'Niepoprawna odpowiedź. Spróbuj jeszcze raz.'
-prompt_success = f'Poprawna odpowiedź! Gratuluję poprawna liczba to:' \
-                 f' {number_to_guess}. Udało się po {user_tries} próbach.'
 user_guess = ''
+
+#User feedback based on input
+def get_feedback(correct_number, guess_number):
+    """Returns response in a form of a list based on user guess number
+    :arg guess_number - user guess number
+    :arg correct_number - correct number generated for the game
+    """
+    response = []
+    i = 0
+    for number in guess_number:
+        pass_number = False
+        if correct_number[i] == guess_number[i]:
+            response.append('Fermi')
+            pass_number = True
+        elif number in correct_number[i:] and not pass_number:
+            print(f"ucięta liczba: {correct_number[i:]}")
+            print(f"sprawdzana liczba: {number}")
+            response.append('Piko')
+            #TODO: Piko bug to be fixed
+        i += 1
+    if not response:
+        return 'Beagle'
+    else:
+        shuffle(response)
+        return ' '.join(response)
 
 # Game Mechanics
 while user_tries <= 10:
-    user_guess = input(f'Zgadnij liczbę z zakresu 001 - 999\nPróba {user_tries}: ')
-    user_tries += 1
+    user_guess = input(f'Guess a number in a range of 001 - 999'
+                       f'\nAnswer {user_tries}: ')
+    feedback = get_feedback(correct_number=number_to_guess, guess_number=user_guess)
     if user_guess != number_to_guess:
-        print(prompt_failed)
+        print(feedback)
     elif user_guess == number_to_guess:
+        prompt_success = f'Correct Answer! Congratulations correct number is:' \
+                         f' {number_to_guess}. You succeed after {user_tries} ' \
+                         f'tries.'
         print(prompt_success)
         break
-
-#TODO: User feedback based on input
-
+    user_tries += 1
